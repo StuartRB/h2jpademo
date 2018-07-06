4c870147231ce023bb81c2533c115980955f7fbc
Stuart Burnside <stuartburnside@stuarts-air.home>
2018-07-06 20:56:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#Spring boot; H2; JPA
+
+* start.spring.io
+
+```Gradle : Java :  -> H2, JPA, Rest```
+
+
+
+* application.properties
+
+```spring.h2.console.enabled=true```
+
+* start app
+
+```gradle bootRun```
+
+* open h2 console
+
+```localhost:8080/h2-console```
---
+++ src/main/java/com/example/h2jpademo/H2jpademoApplication.java
@@ -0,0 +1,12 @@
+package com.example.h2jpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2jpademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2jpademoApplication.class, args);
+	}
+}
